0180c845ddbeb3f8b3d319c670dc015bc0b8377b
Kirill Korsun <k.korsun@tinkoff.ru>
2019-05-15 16:27:44 -0300

    change smth in order to init repo

---
+++ themes/even/src/js/main.js
@@ -0,0 +1,25 @@
+import {Even} from './even.js'
+
+import '../css/style.scss'
+
+$(document).ready(function () {
+  Even.backToTop();
+  Even.mobileNavbar();
+  Even.toc();
+  Even.fancybox();
+  Even.changeLocationHref();
+  Even.pluralizeRussian();
+  Even.formatPostDate();
+});
+
+Even.responsiveTable();
+Even.flowchart();
+Even.sequence();
+
+if (window.hljs) {
+  hljs.initHighlighting();
+  Even.highlight();
+} else {
+  Even.chroma();
+}
+
